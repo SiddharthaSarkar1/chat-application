66ef1a0a8ab1d41f0c2f8e2d09a7dba2c04d09c2
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-26 03:05:52 +0530

    setting up socket.io connections and functionalities

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+import { app, server } from "./socket/socket.js";
+
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 5000;
 
@@ -26,7 +27,7 @@ app.use("/api/users", userRoutes);
 //   res.json({ message: "Hello World" });
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
