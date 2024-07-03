1478cb350d33c82abaca9890ed7579b02723ebc0
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-07-03 23:24:32 +0530

    build frontend code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -9,6 +10,8 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // app.get("/", (req, res) => {
 //   res.json({ message: "Hello World" });
